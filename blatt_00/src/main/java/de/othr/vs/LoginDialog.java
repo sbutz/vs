24d332fb4b177c1aedc543b09b174ba6d1eac3eb
Stefan Butz <stefan260699@gmail.com>
2021-03-29 17:39:52 +0200

    Blatt 0

---
+++ blatt_00/src/main/java/de/othr/vs/LoginDialog.java
@@ -0,0 +1,53 @@
+package de.othr.vs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class LoginDialog extends JFrame {
+
+	JTextField usernameField;
+	JTextField passwordField;
+	JButton exitBtn;
+	JButton loginBtn;
+
+	public LoginDialog() {
+		super("Anmelden");
+
+		this.setLayout(new GridLayout(3,2));
+
+		this.add(new JLabel("Username", JLabel.LEFT));
+		this.usernameField = new JTextField();
+		this.add(this.usernameField);
+
+		this.add(new JLabel("Password", JLabel.LEFT));
+		this.passwordField = new JTextField();
+		this.add(this.passwordField);
+
+		this.exitBtn = new JButton("Exit");
+		this.exitBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		this.add(exitBtn);
+
+		this.loginBtn = new JButton("Login");
+		this.loginBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Username: " +
+					usernameField.getText());
+				System.out.println("Password: " +
+					passwordField.getText());
+			}
+		});
+		this.add(loginBtn);
+
+		this.setPreferredSize(new Dimension(400, 100));
+		this.pack();
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ blatt_00/src/main/java/de/othr/vs/Start.java
@@ -0,0 +1,13 @@
+package de.othr.vs;
+
+import de.othr.vs.LoginDialog;
+
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Es hat geklappt! Hallo Welt!");
+
+	new LoginDialog();
+    }
+
+}
