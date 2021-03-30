476c706ce4473d477b77ed8bd4edcfd02ce3fc82
Stefan Butz <stefan260699@gmail.com>
2021-03-30 08:35:37 +0200

    change identing

--- blatt_01_a/src/main/java/de/othr/vs/ParkingGarage.java
+++ blatt_01_a/src/main/java/de/othr/vs/ParkingGarage.java
@@ -4,35 +4,35 @@ import de.othr.vs.Car;
 
 class ParkingGarage {
 
-	int slots;
-	int reserved;
+    int slots;
+    int reserved;
 
-	Object monitor;
+    Object monitor;
 
-	ParkingGarage(int slots) {
-		this.slots = slots;
-		this.reserved = 0;
+    ParkingGarage(int slots) {
+        this.slots = slots;
+        this.reserved = 0;
 
-		this.monitor = new Object();
-	}
+        this.monitor = new Object();
+    }
 
-	void enter(Car c) throws InterruptedException {
-		synchronized (this.monitor) {
-			while (this.reserved >= this.slots) {
-				System.out.println("  Waiting to enter " +
-					c.getLicensePlate());
-				this.monitor.wait();
-			}
-			this.reserved++;
-		}
-	}
+    void enter(Car c) throws InterruptedException {
+        synchronized (this.monitor) {
+            while (this.reserved >= this.slots) {
+                System.out.println("  Waiting to enter " +
+                    c.getLicensePlate());
+                this.monitor.wait();
+            }
+            this.reserved++;
+        }
+    }
 
-	void leave(Car c) {
-		synchronized (this.monitor) {
-			if (this.reserved > 0) {
-				reserved--;
-				monitor.notify();
-			}
-		}
-	}
+    void leave(Car c) {
+        synchronized (this.monitor) {
+            if (this.reserved > 0) {
+                reserved--;
+                monitor.notify();
+            }
+        }
+    }
 }
--- blatt_01_a/src/main/java/de/othr/vs/Simulation.java
+++ blatt_01_a/src/main/java/de/othr/vs/Simulation.java
@@ -3,16 +3,16 @@ package de.othr.vs;
 import de.othr.vs.ParkingGarage;
 
 class Simulation {
-	Simulation() throws InterruptedException {
-		ParkingGarage garage = new ParkingGarage(5);
+    public static void main(String[] args) throws InterruptedException {
+        ParkingGarage garage = new ParkingGarage(5);
 
-		for (int i = 0; i < 20; i++) {
-			Thread t = new Thread(new Car("R-FH-" + i, garage));
-			t.setDaemon(true);
-			t.start();
-		}
+        for (int i = 0; i < 20; i++) {
+            Thread t = new Thread(new Car("R-FH-" + i, garage));
+            t.setDaemon(true);
+            t.start();
+        }
 
-		Thread.sleep(10000);
-		System.out.println("Ende der Simulation");
-	}
+        Thread.sleep(10000);
+        System.out.println("Ende der Simulation");
+    }
 }
